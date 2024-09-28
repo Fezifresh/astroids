4fe866f9570a2363fdcd9310bd3b1cdab69d4775
Fezifresh <felix.fassig@googlemail.com>
2024-09-28 22:51:12 +0200

    screen set up

--- main.py
+++ main.py
@@ -3,7 +3,25 @@
 # throughout this file
 import pygame
 
+from constants import *
+
 def main():
+    pygame.init
+    screen = pygame.display.set_mode(size=(SCREEN_WIDTH, SCREEN_HEIGHT))
+    # sets the screen size
+
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return
+        # makes the close-button work
+        
+        pygame.Surface.fill(screen, (0, 0, 0))
+        # opens a black screen
+        pygame.display.flip()
+        # refreshes the screen
+
+
     print ("Starting asteroids!")
 
 if __name__ == "__main__":
