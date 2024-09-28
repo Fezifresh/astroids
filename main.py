6c36732a630a77080273b8a28b285c58f0d6b92a
Fezifresh <felix.fassig@googlemail.com>
2024-09-28 23:16:04 +0200

    Hitbox and FPS set up

--- main.py
+++ main.py
@@ -2,7 +2,6 @@
 # the open-source pygame library
 # throughout this file
 import pygame
-
 from constants import *
 
 def main():
@@ -10,19 +9,23 @@ def main():
     screen = pygame.display.set_mode(size=(SCREEN_WIDTH, SCREEN_HEIGHT))
     # sets the screen size
 
+    clock = pygame.time.Clock()
+    dt = 0
+    # sets up the clock for framerate and a delta variable
+
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         # makes the close-button work
         
-        pygame.Surface.fill(screen, (0, 0, 0))
+        pygame.Surface.fill(screen, (0, 0, 51))
         # opens a black screen
         pygame.display.flip()
         # refreshes the screen
 
-
-    print ("Starting asteroids!")
+        dt = clock.tick(60) / 1000
+        # FPS = 60
 
 if __name__ == "__main__":
     main()
