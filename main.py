70786fe38237768ac7c6aa22778d0b5f06d6a057
Fezifresh <felix.fassig@googlemail.com>
2024-09-30 22:27:57 +0200

    Player shape added

--- main.py
+++ main.py
@@ -3,16 +3,19 @@
 # throughout this file
 import pygame
 from constants import *
+from player import *
 
 def main():
     pygame.init
     screen = pygame.display.set_mode(size=(SCREEN_WIDTH, SCREEN_HEIGHT))
     # sets the screen size
 
+    ship = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+
     clock = pygame.time.Clock()
     dt = 0
     # sets up the clock for framerate and a delta variable
-
+    
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
@@ -20,12 +23,18 @@ def main():
         # makes the close-button work
         
         pygame.Surface.fill(screen, (0, 0, 51))
-        # opens a black screen
+        # opens a dark blue screen
+        
+        ship.draw(screen)
+        # draws the player
+        
         pygame.display.flip()
         # refreshes the screen
-
+        
         dt = clock.tick(60) / 1000
         # FPS = 60
+
+        
 
 if __name__ == "__main__":
     main()
